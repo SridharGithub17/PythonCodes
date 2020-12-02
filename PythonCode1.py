1f25ee1616d338882b236bdd3d4fc17fcdf8aba6
sridhar <das.sridhar1@gmail.com>
2020-12-01 23:04:16 -0500

    "New file added 12012020

---
+++ PythonCode1.py
@@ -0,0 +1,16 @@
+
+#!/usr/bin/env python
+# -*- coding: encoding -*-
+
+def main():
+  # Connect to snowflake
+  print("Testing")
+  a=34
+  b=55
+  print(a|b)
+  print("Sridhar is a good boy")
+  print(a&b)
+  print("Testing done")
+  
+if __name__ == "__main__":
+  main()
