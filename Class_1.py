c2a1b18a9964ff1091ae6956716f89a1ecd910be
sridhar <das.sridhar1@gmail.com>
2021-01-15 13:47:42 -0500

    "Commit Class file"

--- Class_1.py
+++ Class_1.py
@@ -1,5 +1,12 @@
 #/bin/usr/env python
 
+#Scopes
+#4 nested scopes whose namespaces are directly accessible:
+#the innermost scope, which is searched first, contains the local names
+#the scopes of any enclosing functions, which are searched starting with the nearest enclosing scope, contains non-local, but also non-global names
+#the next-to-last scope contains the current module’s global names
+#the outermost scope (searched last) is the namespace containing built-in names
+
 def main():
     print("In the main block")
 
