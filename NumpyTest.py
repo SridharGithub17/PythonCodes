6ae0c62d8c0336f360b52662f1bdbf90a95c9b13
Sridhar Das <das.sridhar1@gmail.com>
2023-09-12 23:23:20 -0400

    Numpy test

---
+++ NumpyTest.py
@@ -0,0 +1,21 @@
+#/bin/usr python
+import numpy as np
+
+def NumPy_Test():    
+    try:
+        a=np.array([1, 2, 3.0]) 
+        print(a)
+
+    except ZeroDivisionError: 
+        print('There is some issue in the try block')
+    else:
+        print("In the else block")
+    finally:
+        print("Other errors, need diagnosis")
+
+
+def main():
+    NumPy_Test()
+
+if __name__ == "__main__":
+    main()
